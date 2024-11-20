d315f4e4f84666eab1d7d735e3b30639876713b7
Alberto <albertojunior848423803@gmail.com>
2024-11-21 01:01:56 +0300

    changes

--- src/StackArrays.java
+++ src/StackArrays.java
@@ -4,14 +4,13 @@ public class StackArrays {
     int maxSize;
     int top;
     int[] stackArray;
-
-
+    
     public StackArrays(int size){
         maxSize = size;
         stackArray = new  int[maxSize];
         top = -1;
     }
-    
+
     public static void main (String[] args){
 
     }
