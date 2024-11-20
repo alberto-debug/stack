b2a8e5b47d0feb21a52c8bcec87ccf96db71da3a
Alberto <albertojunior848423803@gmail.com>
2024-11-20 23:14:57 +0300

    changes

--- src/StackArrays.java
+++ src/StackArrays.java
@@ -1,16 +1,9 @@
 
 public class StackArrays {
 
-    int  maxSize;
+    int maxSize;
     int top;
     int[] stackArray;
-
-    // Constructor to sizinitialize the stack with a specified size
-    public StackArrays(int size){
-        maxSize = size;
-        stackArray = new int[maxSize];
-        top = -1; // Initialize the top pointer to -1 (empty stack)
-    }
 
 
     public static void main (String[] args){
