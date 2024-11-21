945f934ace56e3bcefb2843e31f87110a2e049f4
Alberto <albertojunior848423803@gmail.com>
2024-11-21 13:08:11 +0300

    changes

--- src/StackArrays.java
+++ src/StackArrays.java
@@ -3,25 +3,10 @@ public class StackArrays {
 
     int maxSize;
     int top;
-     int[] stackArray;
-
-     public StackArrays(int size){
-         maxSize  = size;
-         stackArray= new int[maxSize];
-         top = -1;
-     }
-
-     //check if the stack is empty
-    public boolean isEmpty(){
-         return (top == -1);
-    }
-
-    public boolean isFull(){
-         return (top == maxSize -1);
-    }
+    int[] stackArray;
 
     public static void main (String[] args){
-
+        
     }
 
 }
